63d10a6c0c42e9246c27a28ebfe79aeed6dadbb1
Leo3k <leonardosantunes@yahoo.com>
2022-06-16 11:49:09 -0300

    magin-top

--- eli_class.php
+++ eli_class.php
@@ -133,7 +133,7 @@ include('verifica_login.php');
                             <button type="button" class="btn btn-success" onclick="displayLessons('jan')">January</button>
                         
                         <br><br>
-                        <table id="jan" class="table table-bordered table-hover" style="background-color: white; width:400px; margin:auto;">
+                        <table id="jan" class="table table-bordered table-hover" style="background-color: white; width:400px; margin:auto; margin-top:40px;">
                             <thead>
                                 <tr>
                                 <th>Dia</th>
@@ -205,7 +205,7 @@ include('verifica_login.php');
                                 </tr>
                             </tbody>
                         </table>
-                        <table id="feb" class="table table-bordered table-hover" style="background-color: white; width:400px; margin:auto;">
+                        <table id="feb" class="table table-bordered table-hover" style="background-color: white; width:400px; margin:auto; margin-top:40px;">
                             <thead>
                                 <tr>
                                 <th>Dia</th>
@@ -289,7 +289,7 @@ include('verifica_login.php');
                                 </tr> 
                             </tbody>
                         </table>
-                        <table id="mar" class="table table-bordered table-hover" style="background-color: white; width:400px; margin:auto;">
+                        <table id="mar" class="table table-bordered table-hover" style="background-color: white; width:400px; margin:auto; margin-top:40px;">
                             <thead>
                                 <tr>
                                 <th>Dia</th>
@@ -368,7 +368,7 @@ include('verifica_login.php');
                                 </tr>
                             </tbody>
                         </table>
-                        <table id="apr" class="table table-bordered table-hover" style="background-color: white; width:400px; margin:auto;">
+                        <table id="apr" class="table table-bordered table-hover" style="background-color: white; width:400px; margin:auto; margin-top:40px;">
                             <thead>
                                 <tr>
                                 <th>Dia</th>
@@ -428,7 +428,7 @@ include('verifica_login.php');
                                 </tr> 
                             </tbody>
                         </table>
-                        <table id="may" class="table table-bordered table-hover" style="background-color: white; width:400px; margin:auto;">
+                        <table id="may" class="table table-bordered table-hover" style="background-color: white; width:400px; margin:auto; margin-top:40px;">
                             <thead style="margin:auto;">
                                 <tr>
                                 <th>Dia</th>
@@ -494,7 +494,7 @@ include('verifica_login.php');
                                 </tr>
                             </tbody>
                         </table>
-                        <table id="jun" class="table table-bordered table-hover" style="background-color: white; width:400px; margin:auto;">
+                        <table id="jun" class="table table-bordered table-hover" style="background-color: white; width:400px; margin:auto; margin-top:40px;">
                             <thead>
                                 <tr>
                                 <th>Dia</th>
