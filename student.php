65a06fc63052cfe26d752b2c827450f451e9099e
Leo3k <leonardosantunes@yahoo.com>
2023-01-02 20:06:33 -0300

    ht

--- student.php
+++ student.php
@@ -3,7 +3,7 @@ session_start();
 include "navbar.php"; 
 
 if (!isset($_SESSION['username'])) {
-  header("Location: index.php");
+  echo "<script>window.location.href='index.php'</script>";
 }
 ?>
 <style>
