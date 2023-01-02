26352178964263b3b3d30bff60ab035f605afaaf
Leo3k <leonardosantunes@yahoo.com>
2023-01-02 19:04:25 -0300

    finally

--- connection.php
+++ connection.php
@@ -7,8 +7,8 @@
 
 //Production
 define('HOST', '108.167.132.22');
-define('USUARIO', 'ingl814_leo');
-define('SENHA', 'eu<3MACEIO');
-define('DB', 'ingl814_site');
+define('USUARIO', 'ingle814_leo');
+define('SENHA', 'eu<#MACEIO');
+define('DB', 'ingle814_site');
 
 $conn = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não conectado'); 
