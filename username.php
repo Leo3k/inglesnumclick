43045b10d71bc72d81b13d6a68c280ff02267605
Leo3k <leonardosantunes@yahoo.com>
2023-01-02 18:40:40 -0300

    678

--- username.php
+++ username.php
@@ -3,6 +3,20 @@ session_start();
 include "navbar.php"; 
 ?>
 <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<div style="border:1px solid white;width:380px; border-radius:5px; margin:auto; text-align:center; margin-top:5px;padding:5px;">
+    <figure class="text-center mt-3">
+    <blockquote class="blockquote">
+        <h2 style="color:white">What is your username?</p>
+    </blockquote>
+    <figcaption style="color:white" class="blockquote-footer">
+        Type in your username below
+    </figcaption>
+    </figure>
+    <form class="input-group" action="" method="post" autocomplete="off">
+        <input name='username' maxlength="6" style="text-align:center;" type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+        <button type="submit" name="submit" class="btn btn-info">SEND</button>
+    </form>
+</div>
 <?php require 'connection.php';
 
 if (!isset($_SESSION['username'])) {
@@ -33,17 +47,4 @@ if(isset($_POST["submit"])){
     header("Location: student.php");
 }
 ?>
-<div style="border:1px solid white;width:380px; border-radius:5px; margin:auto; text-align:center; margin-top:5px;padding:5px;">
-    <figure class="text-center mt-3">
-    <blockquote class="blockquote">
-        <h2 style="color:white">What is your username?</p>
-    </blockquote>
-    <figcaption style="color:white" class="blockquote-footer">
-        Type in your username below
-    </figcaption>
-    </figure>
-    <form class="input-group" action="" method="post" autocomplete="off">
-        <input name='username' maxlength="6" style="text-align:center;" type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
-        <button type="submit" name="submit" class="btn btn-info">SEND</button>
-    </form>
-</div>
+</body>
